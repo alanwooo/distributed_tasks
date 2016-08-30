cb2c0074c94d496516999b01c6cd14c0ac4562e3
alanwooo <herobzyy@gmail.com>
2016-08-30 20:55:37 +0800

    enhance the function

--- saveFileToDB.py
+++ saveFileToDB.py
@@ -21,11 +21,22 @@ def getFiles(arg, dirname, files):
 def saveToDB(storefile):
     pass 
 
+def getUserDir():
+    for d in os.listdir('./'):
+        if os.path.isdir(d):
+            yield d
+
+def getProjectDir(udir):
+    for p in os.listdir(udir):
+        path = os.path.join(udir, p)
+        if os.path.isdir(path):
+            yield path
+
 def findSourceFile(rootdir):
     os.chdir(rootdir)
-    udirs = [ d for d in os.listdir('./') if os.path.isdir(d) ]
-    pdirs = [ os.path.join(u, p) for u in udirs for p in os.listdir(u) if os.path.isdir(os.path.join(u, p)) ]
-    del udirs
+    #udirs = [ d for d in os.listdir('./') if os.path.isdir(d) ]
+    #pdirs = [ os.path.join(u, p) for u in udirs for p in os.listdir(u) if os.path.isdir(os.path.join(u, p)) ]
+    #del udirs
 
     def insertToDB(name, files):
         rec = {'full_name' : name,
@@ -33,11 +44,15 @@ def findSourceFile(rootdir):
               }
         client.insertOneRecord(rec)
 
-    for pro in pdirs:
-        storefile = []
-        os.path.walk(pro, getFiles, storefile)
-        if pro and storefile:
-            insertToDB(pro, storefile)
+    for udir in getUserDir():
+        for pro in getProjectDir(udir):
+            storefile = []
+            os.path.walk(pro, getFiles, storefile)
+            if pro and storefile:
+                insertToDB(pro, storefile)
+                #print pro 
+                #print storefile
+                #print '\n'
 
 if __name__ == '__main__':
     client = mongo(MONGOSERVER, MONGOPORT)
